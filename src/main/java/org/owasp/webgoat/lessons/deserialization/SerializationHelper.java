bfd17d001995a1283b4887fa26069ea8fb564d66
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-06-17 10:19:25 +0530

    Harness AI Remediation to fix: Semgrep Finding: java.lang.security.audit.object-deserialization.object-deserialization

--- src/main/java/org/owasp/webgoat/lessons/deserialization/SerializationHelper.java
+++ src/main/java/org/owasp/webgoat/lessons/deserialization/SerializationHelper.java
@@ -19,7 +19,65 @@ public class SerializationHelper {
 
   public static Object fromString(String s) throws IOException, ClassNotFoundException {
     byte[] data = Base64.getDecoder().decode(s);
-    ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data));
+    /*
+ * SPDX-FileCopyrightText: Copyright © 2019 WebGoat authors
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+package org.owasp.webgoat.lessons.deserialization;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Base64;
+
+public class SerializationHelper {
+
+  private static final char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+  public static Object fromString(String s) throws IOException, ClassNotFoundException {
+      byte[] data = Base64.getDecoder().decode(s);
+      try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data))) {
+          Object o = ois.readObject();
+          // Add validation of the deserialized object type here
+          if (!(o instanceof Serializable)) {
+              throw new IOException("Deserialized object is not of expected type");
+          }
+          return o;
+      }
+    }
+
+  public static String toString(Serializable o) throws IOException {
+
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    ObjectOutputStream oos = new ObjectOutputStream(baos);
+    oos.writeObject(o);
+    oos.close();
+    return Base64.getEncoder().encodeToString(baos.toByteArray());
+  }
+
+  public static String show() throws IOException {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    DataOutputStream dos = new DataOutputStream(baos);
+    dos.writeLong(-8699352886133051976L);
+    dos.close();
+    byte[] longBytes = baos.toByteArray();
+    return bytesToHex(longBytes);
+  }
+
+  public static String bytesToHex(byte[] bytes) {
+    char[] hexChars = new char[bytes.length * 2];
+    for (int j = 0; j < bytes.length; j++) {
+      int v = bytes[j] & 0xFF;
+      hexChars[j * 2] = hexArray[v >>> 4];
+      hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+    }
+    return new String(hexChars);
+  }
+}
     Object o = ois.readObject();
     ois.close();
     return o;
